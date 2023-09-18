937fae28f61850beb8e6bc2bffc7d790c7c0f077
Shivashankar Rampur <shivashankar.rampur@quadratyx.com>
2023-09-18 09:58:06 +0530

    code updated

--- main.py
+++ main.py
@@ -9,12 +9,3 @@ class testing:
     def sub(self, a, b):
         d=a-b
         return d
-
-
-# test_obj = testing()
-# a=4
-# b=6
-# sum= test_obj.add(a, b)
-# sub= test_obj.sub(a, b)
-# print("addition of {0} and {1} is: {2}".format(a, b,sum))
-# print("substraction of {0} and {1} is: {2}".format(a, b, sub))
